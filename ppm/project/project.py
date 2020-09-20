14b1f315d46643ab0a18c98404a2d0b48e6d0d80
LavaPower <lavapower84@gmail.com>
2020-09-20 16:26:49 +0200

    Improve Project Creation

--- ppm/project/project.py
+++ ppm/project/project.py
@@ -5,6 +5,9 @@ class Project:
     def __init__(self, directory, config):
         self.config = config
         self.directory = directory
+        os.chdir(self.directory)
+        self.ppm_directory = os.path.join(directory, ".ppm")
+        self.python_directory = None
         self.project_file = os.path.join(directory, ".ppm", "project.json")
         if os.path.exists(self.project_file):
             with open(self.project_file, "r") as f:
@@ -12,13 +15,6 @@ class Project:
         else:
             self.project_info = self.create()
             self.save()
-
-    def create(self):
-        name = input("Project Name : ")
-        author = input("Author : ")
-        if self.config.get("git", False):
-            os.system("git init")
-        return { "name": name, "author": author }
     
     def save(self):
         with open(self.project_file, "w") as f:
@@ -29,3 +25,50 @@ class Project:
         for k, v in self.project_info.items():
             text.append(k.capitalize() + " : " + v)
         return "\n".join(text)
+
+    def create(self):
+        name = input("Project Name : ")
+        author = input("Author : ")
+        email = input("Email : ")
+        description = input("Description : ")
+        self.python_directory = os.path.join(self.directory, name.lower())
+        os.makedirs(self.python_directory)
+        with open(os.path.join(self.directory, "README.md"), "w") as f:
+            f.write("# "+name+"\n\n"+description+"\n")
+        with open(os.path.join(self.python_directory, "__init__.py"), "w") as f:
+            f.write("__version__ = \"1.0.0\"")
+        with open(os.path.join(self.directory, "setup.py"), "w") as f:
+            f.write("""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+
+import {namelower}
+
+setup(
+
+    name='{name}',
+
+    version={namelower}.__version__,
+
+    packages=find_packages(),
+    author="{author}",
+    author_email="{email}",
+    description="{description}",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+
+    include_package_data=True,
+
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers.
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning"
+    ],
+    install_requires=[]
+
+ 
+)""".format(name=name, namelower=name.lower(), author=author, email=email, description=description))
+
+        return { "name": name, "author": author, "email": email, "description": description }
